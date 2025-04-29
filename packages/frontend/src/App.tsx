97fb2795ab0634f52f7ae4f132b8ba2844c38418
Huy (Akira) Tran <nhatgiahuyt@gmail.com>
2025-04-29 12:26:15 +0700

    feat/auth-credentials-signup-login

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,14 +1,15 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Toaster } from "react-hot-toast";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import Layout from "./components/layout/Layout";
 import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
 import Callback from "./pages/auth/Callback";
 import About from "./pages/About";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
-
 
 // Pages
 const Home = () => (
@@ -24,6 +25,7 @@ export default function App() {
   return (
     <AuthProvider>
       <Routes>
+        {/* Public: Login */}
         <Route
           path="/login"
           element={
@@ -32,6 +34,16 @@ export default function App() {
             </ProtectedRoute>
           }
         />
+        {/* Public: Signup */}
+        <Route
+          path="/signup"
+          element={
+            <ProtectedRoute requireAuth={false}>
+              <Signup />
+            </ProtectedRoute>
+          }
+        />
+        {/* OAuth Callback */}
         <Route
           path="/auth/callback"
           element={
@@ -40,12 +52,12 @@ export default function App() {
             </ProtectedRoute>
           }
         />
+
         {/* Public routes with layout */}
         <Route element={<Layout />}>
           <Route path="/about" element={<About />} />
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
-          
         </Route>
 
         {/* Protected routes with layout */}
@@ -58,7 +70,6 @@ export default function App() {
           }
         >
           <Route index element={<Home />} />
-         
         </Route>
       </Routes>
       <Toaster position="top-center" />
